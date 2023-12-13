ab16260f185d1c0055b385480bb4206cff2a9418
Julie Cortis <cortis345@gmail.com>
2023-12-13 22:04:12 +0100

    created main script and added board and printing board function

---
+++ python/main.py
@@ -0,0 +1,19 @@
+board = ["-","-","-",
+         "-","-","-",
+         "-","-","-"]
+
+def printBoard():
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    
+
+printBoard()
+
+#player input
+#check for win or tie
+#switch player
+#check for win or tie again
+
+
+
