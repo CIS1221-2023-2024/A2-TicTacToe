d87733db69548e449db09192496ce79a1748b6a4
Julie Cortis <cortis345@gmail.com>
2023-12-17 21:52:51 +0100

    Added player input function and basic gameplay function

--- python/main.py
+++ python/main.py
@@ -1,19 +1,43 @@
-board = ["-","-","-",
-         "-","-","-",
-         "-","-","-"]
+board = ["1","2","3",
+         "4","5","6",
+         "7","8","9"]
 
-def printBoard():
-    print(board[0] + " | " + board[1] + " | " + board[2])
-    print(board[3] + " | " + board[4] + " | " + board[5])
-    print(board[6] + " | " + board[7] + " | " + board[8])
-    
+currentPlayer = "X"
+gameRunning = True
+winner = None
 
-printBoard()
+def printBoard(brd):
+    print(brd[0] + " | " + brd[1] + " | " + brd[2])
+    print("-" * 10)
+    print(brd[3] + " | " + brd[4] + " | " + brd[5])
+    print("-" * 10)
+    print(brd[6] + " | " + brd[7] + " | " + brd[8])
 
 #player input
+def playerInp(brd):
+    inpVal = True
+    while inpVal:
+        inp = input("Enter the location to place marker in ")
+        if(inp.isdigit()):
+            inpInt = int(inp)
+            if (inpInt >= 1 and inpInt<=9):
+                if brd[inpInt - 1] == inp:
+                    brd[(inpInt - 1)] = currentPlayer
+                    inpVal = False
+                else:
+                    print("Location already taken!")
+            else:
+                print("Integer entered is not a location on the board!")
+        else:
+            print("Opps the input entered in not an integer!")
+            
 #check for win or tie
 #switch player
 #check for win or tie again
 
-
-
+def gamePlay():
+    while gameRunning == True:
+        printBoard(board)
+        playerInp(board)
+        
+gamePlay
